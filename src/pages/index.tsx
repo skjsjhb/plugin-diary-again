c05037ef0c59f21abf008d3241fa1bf121d8779c
skjsjhb <skjsjhb@outlook.com>
2025-03-28 12:55:31 +0800

    Update Fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import "@fontsource-variable/jetbrains-mono"
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
