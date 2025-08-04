b49b051858fe2d91f6306075315a099fa8a5bc8e
skjsjhb <skjsjhb@outlook.com>
2025-08-05 04:42:49 +0800

    Add 12-3, 12-4

--- quote-replace.js
+++ quote-replace.js
@@ -5,7 +5,9 @@ export default function replaceQuotes() {
     return function (tree) {
         findAndReplace(tree, [
             ["“", "『"],
-            ["”", "』"]
+            ["”", "』"],
+             ["‘", "「"],
+            ["’", "」"]
         ]);
     }
 }
